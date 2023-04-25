4fe65f79b8c6bc19d0dd3d8d08d84b6a1a81cc35
Andrewk57 <112024432+Andrewk57@users.noreply.github.com>
2023-04-25 17:04:57 -0400

    added tutorial and level2,

---
+++ Assets/scripts/tutorial.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorial : MonoBehaviour
+{
+    public GameObject[] popups;
+    private int popUpIndex;
+    private void Update()
+    {
+        for (int i = 0; i < popups.Length; i++)
+        {
+            if (i == popUpIndex)
+            {
+                popups[i].SetActive(true);
+            }
+            else
+            {
+                popups[i].SetActive(false);
+            }
+        }
+        if (popUpIndex == 0)
+        {
+            // movement
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+            {
+                Debug.Log("Step one: move done");
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 1)
+        {
+            // shooting enemy
+            if (GameObject.Find("1") == null)
+            {
+                Debug.Log("Step 2: Kill enemy done");
+                popUpIndex++;
+            }
+        }
+        else if (popUpIndex == 2)
+        {
+            // shooting barrel
+            if (GameObject.Find("model") == null)
+            {
+                Debug.Log("Step 3: TNT expload completed");
+                popUpIndex++;
+            }
+        }
+
+    }
+}
